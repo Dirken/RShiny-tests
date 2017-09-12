a9c7e4ff3ac582d351e4406d39c6e216c56b7438
Dirken <ricardmeyerhofer@gmail.com>
2017-09-12 23:39:21 +0200

    reorganization and starting to do something with rhandsometable

---
+++ table-examples/dt-table-test/dtable/server.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(DT)
+
+shinyServer(function(input, output, session) {
+  x <- DT::datatable(iris, 
+                     extensions = "Buttons",
+                     filter = list(position = 'top', clear = FALSE),
+                     options = list(
+                       regex = TRUE,
+                       searchHighlight = TRUE,
+                       search = list(regex = TRUE),
+                       columnDefs = list(list(className = 'dt-center', targets = 5)),
+                       pageLength = 5,
+                       lengthMenu = c(5, 10, 15, 20),
+                       dom = 'Bfrtip',
+                       buttons = c('copy', 'csv', 'excel', 'pdf', 'print', I('colvis'))
+                     ))
+  
+  output$x1 = renderDataTable(x, selection = 'none', server=TRUE)
+  
+  
+  
+  proxy = dataTableProxy('x1')
+  
+  observeEvent(input$x1_cell_edit, {
+    info = input$x1_cell_edit
+    str(info)
+    i = info$row
+    j = info$col
+    v = info$value
+    str('peta')
+    
+    x[i, j] <<- DT:::coerceValue(v, x[i, j])
+    str('peta')
+    replaceData('proxy', x, resetPaging = FALSE)
+  })
+  
+})
